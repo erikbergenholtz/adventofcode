141f8e6a21fd92d1f0ba4f96eef48614b023dd0a
Erik Bergenholtz <ebz@bth.se>
2017-12-04 12:25:58 +0100

    Add solution to Haskell/p2/02/2017

--- 2017/02/haskell/solution.hs
+++ 2017/02/haskell/solution.hs
@@ -1,16 +1,38 @@
 import System.Environment
 import System.IO
 import Data.Char
-import Data.List
 
-rowSum :: [String] -> Int
+arrayRead :: [[String]] -> [[Int]]
+arrayRead [] = []
+arrayRead (xs:xss) = (map read xs) : arrayRead xss
+
+rowSum :: [Int] -> Int
 rowSum []  = 0
-rowSum xs  = abs ((maximum nums) - (minimum nums))
-    where nums = map read xs
+rowSum xs  = abs ((maximum xs) - (minimum xs))
+
+findDivisible :: [Int] -> Int -> Int
+findDivisible _  0 = 0
+findDivisible [] _ = 0
+findDivisible (y:ys) x
+    | x == y         = findDivisible ys x
+    | h `rem` l == 0 = h `quot` l
+    | otherwise      = findDivisible ys x
+    where h          = max x y
+          l          = min x y
+
+part2 :: [Int] -> Int
+part2 (x:xs)
+    | res > 0   = res
+    | otherwise = part2 xs
+    where res   = findDivisible xs x
 
 main :: IO()
 main = do
     args <- getArgs
     f <- openFile (head args) ReadMode
     contents <- hGetContents f
-    putStrLn $ show $ sum $ map rowSum (map words ( lines contents ))
+    let ls = arrayRead $ map words $ lines contents
+    -- Part 1
+    putStrLn $ show $ sum $ map rowSum ls
+    -- Part 2
+    putStrLn $ show $ sum $ map part2 ls
