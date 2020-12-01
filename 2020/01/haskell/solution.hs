4d466b75537ec69683caf10f69ef83ca71eaf4a3
Erik Bergenholtz <ebz@bth.se>
2020-12-01 22:36:48 +0100

    Day 1 - Part 2. It ain't pretty

--- 2020/01/haskell/solution.hs
+++ 2020/01/haskell/solution.hs
@@ -14,6 +14,18 @@ test (x:xs) (y:ys) ys'
     | x + y == 2020 = (Just (x*y))
     | otherwise     = test (x:xs) ys ys'
 
+solve2 :: [Int] -> (Maybe Int)
+solve2 xs = test2 xs xs xs (tail xs) (tail xs)
+
+test2 :: [Int] -> [Int] -> [Int] -> [Int] -> [Int] -> (Maybe Int)
+test2 [] _ _ _ _ = Nothing
+test2 (x:xs) [] [] ys' zs'  = test2 xs ys' zs' ys' zs'
+test2 (x:xs) [] (z:zs) ys' zs'  = test2 xs ys' zs' ys' zs'
+test2 (x:xs) (y:ys) [] ys' zs'  = test2 (x:xs) ys zs' ys' zs'
+test2 (x:xs) (y:ys) (z:zs) ys' zs'
+    | x + y + z == 2020 = (Just (x*y*z))
+    | otherwise     = test2 (x:xs) (y:ys) zs ys' zs'
+
 main :: IO()
 main = do
     argv     <- getArgs
@@ -21,3 +33,4 @@ main = do
     contents <- hGetContents file
     let ds = sort $ map read (words contents) :: [Int]
     putStrLn $ show $ solve ds
+    putStrLn $ show $ solve2 ds
